8806ff6cac3a834e2938c1679af77a4bb5e7b7f1
Edlose <edwin.cbalbin@gmail.com>
2023-03-16 16:58:11 -0500

    feat(home): :art: Home added

---
+++ src/features/home/Home.tsx
@@ -0,0 +1,60 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, {FC} from 'react';
+import {ScrollView, StyleSheet, Text, View, useColorScheme} from 'react-native';
+import Colors from '../shared/colors';
+import YourPoints from './components/YourPoints';
+import {Container, SizedBox} from '../shared/components';
+import YourMovements from './components/YourMovements';
+
+type HomeViewProps = {};
+const HomeView: FC<HomeViewProps> = _ => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <ScrollView contentInsetAdjustmentBehavior="automatic">
+      <Container>
+        <View>{_buildHi()}</View>
+        <SizedBox height={20} />
+        <YourPoints />
+        <SizedBox height={20} />
+        <YourMovements />
+        <SizedBox height={200} />
+      </Container>
+    </ScrollView>
+  );
+};
+
+const _buildHi = () => {
+  return (
+    <View>
+      <Text style={styles.title}>Bienvenido de vuelta!</Text>
+      <Text>Rubén Rodriguez</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  title: {
+    fontWeight: 'bold',
+    color: Colors.black,
+    fontSize: 16,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default HomeView;
