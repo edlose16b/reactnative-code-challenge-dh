86ac0767c7e84b745c57553c76b7f50c0b0b91f9
Edlose <edwin.cbalbin@gmail.com>
2023-03-17 22:19:00 -0500

    fix: :ambulance: deleting useless line

--- index.js
+++ index.js
@@ -5,7 +5,5 @@
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
-import {LogBox} from 'react-native';
-LogBox.ignoreAllLogs(); //Ignore all log notifications
-npm;
+
 AppRegistry.registerComponent(appName, () => App);
